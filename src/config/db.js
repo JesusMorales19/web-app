f7a3bb75a7874172bfd6ec3aa6f73ac072ab7d0a
JesusMorales19 <138167542+JesusMorales19@users.noreply.github.com>
2024-04-17 08:44:26 -0600

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-const MONGO_URL = "mongodb+srv://fernanda:1234@cluster0.6j8qpyt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const MONGO_URL = "mongodb+srv://jesuhernan232:jesus1234@cluster1.by7rknx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1";
 
 //Este archivo es para crear la conexion a base de datos
 export const connectDB = async () => {
